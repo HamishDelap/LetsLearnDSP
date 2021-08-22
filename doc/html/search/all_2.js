6acddbe33e8b8c7d5ca6a464b38a671643da0788
HamishDelap <hamish.delap@gmail.com>
2021-08-22 22:04:26 +0100

    Added test JUCE plugin directory and cleaned some files up

---
+++ doc/html/search/all_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['tanhdistortion_0',['tanhDistortion',['../lldsp_8h.html#a9cce4d1e23c40c714224b63a590c8b9a',1,'lldsp.h']]]
+];
---
+++ doc/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['readme_0',['README',['../md__c___users_hamis__documents__dev__audio_development__lets_learn_d_s_p__r_e_a_d_m_e.html',1,'']]]
+];
