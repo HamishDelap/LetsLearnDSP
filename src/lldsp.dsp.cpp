a3a119f61ee5ac709ad33dca9f573c361d337ce9
HamishDelap <hamish.delap@gmail.com>
2023-08-26 14:29:49 +0100

    Added BiQuad Lowpass filter implementation.
    Needs to be properly implemented into the UI but it works!

--- src/lldsp.dsp.cpp
+++ src/lldsp.dsp.cpp
@@ -1,5 +1,8 @@
 #include "lldsp.dsp.h"
 
+#include <math.h>
+#include <cmath>
+
 // CombFilter
 lldsp::dsp::CombFilter::CombFilter(double delayTime, double gain, double sampleRate) : m_DelayBuffer(sampleRate)
 {
@@ -34,4 +37,26 @@ double lldsp::dsp::AllPassFilter::Process(double sample)
 	double postDelayVal = delayedVal * (1 - m_Gain * m_Gain);
 
 	return postDelayVal + sample * (-1 * m_Gain);
+}
+
+
+double lldsp::dsp::BiQuadFilter::Process(double sample)
+{
+	double output = (sample * m_a0) + m_z1;
+	m_z1 = (sample * m_a1) + m_z2 - (output * m_b1);
+	m_z2 = (sample * m_a2) - (output * m_b2);
+	return output;
+}
+
+void lldsp::dsp::BiQuadFilter::SetCutoff(double Fc, double Q, double peakGain)
+{
+	double norm;
+	double V = pow(10, abs(peakGain) / 20);
+	double K = tan(M_PI * Fc / m_sampleRate);
+	norm = 1 / (1 + K / Q + K * K);
+	m_a0 = K * K * norm;
+	m_a1 = 2 * m_a0;
+	m_a2 = m_a0;
+	m_b1 = 2 * (K * K - 1) * norm;
+	m_b2 = (1 - K / Q + K * K) * norm;
 }
--- src/lldsp.dsp.h
+++ src/lldsp.dsp.h
@@ -31,4 +31,26 @@ namespace lldsp::dsp
 		int m_SampleDelay;
 		double m_Gain;
 	};
+
+	// Based on: https://www.earlevel.com/main/2003/02/28/biquads/ 
+	class BiQuadFilter
+	{
+	public:
+		BiQuadFilter() : m_sampleRate(44100) {}
+		BiQuadFilter(double sampleRate) : m_sampleRate(sampleRate) {}
+		
+		double Process(double sample);
+
+		void SetCutoff(double Fc, double Q, double peakGain);
+
+	private:
+		double m_sampleRate;
+		double m_z1 = 0;
+		double m_z2 = 0;
+		double m_a0 = 0.03641371651958762;
+		double m_a1 = 0.07282743303917524;
+		double m_a2 = 0.03641371651958762;
+		double m_b1 = -1.4440155783021371;
+		double m_b2 = 0.5896704443804874;
+	};
 }
